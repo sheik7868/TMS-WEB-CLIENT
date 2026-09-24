d2d477a8836cd66c3c61b09893f16819dcfefb76
sheik <sheik.ik2000@gmail.com>
2026-09-24 16:00:59 +0530

    add

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = class Configuration {
+    GATEWAY_URL = 'http://localhost:3000/api/rest/tms/dataaccess/1.0.0/';
+    GATEWAY_USERNAME = 'QP0192923232LD';
+    GATEWAY_PASSWORD = '927NBGJJ0283HKA74782';
+    HEADERS = {
+        MERCHANT_KEY: 'BASE-MERCHANT-KEY',
+        TENANT_KEY: 'BASE-TENANT-KEY'
+    }
+}
